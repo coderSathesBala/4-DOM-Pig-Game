6de87120719a7af16c3e9ea3914304b6a0ed6c95
Sathes Balasingam <sathes352@gmail.com>
2017-04-03 17:47:34 -0400

    Javascript

--- app.js
+++ app.js
@@ -9,11 +9,72 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6) + 1
-console.log(dice);
+
+document.querySelector('.dice').style.display = 'none'
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+
+  // 1. Random Number
+  var dice = Math.floor(Math.random() * 6) + 1
+
+  //2. Display the result
+  var diceDOM = document.querySelector('.dice');
+  diceDOM.style.display = 'block';
+  diceDOM.src = 'dice-' + dice + '.png';
+
+  //3. Update the round score IF the rolled number was NOT a 1
+  if (dice !== 1) {
+    roundScore += dice;
+    document.querySelector('#current-' + activePlayer).textContent = roundScore;
+  } else {
+    //next player
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    document.querySelector('.dice').style.display = 'none'
+  }
+});
+
+document.querySelector('.btn-hold').addEventListener('click' function() {
+    //current score to total score
+
+    // update the ui
+
+    // check if the player won the game
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//s
